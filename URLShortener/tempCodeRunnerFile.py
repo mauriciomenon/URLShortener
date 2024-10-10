6ca848e755391455df191a67562b0d3b0cf2ae34
menon <mauriciomenon@users.noreply.github.com>
2024-10-10 09:59:44 -0300

    reorganizacao do old_stable

--- URLShortener/tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
-
-        # Configurar o tema específico do Windows, cores suavizadas para estilo Windows 10
-        self.setStyleSheet(f"""
-            QWidget {{
-                background-color: #e6e6e6;
-                font-family: 'Arial';
-                font-size: {font_size};
-            }}
