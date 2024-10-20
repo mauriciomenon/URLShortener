7cb0fa696c448172649c21e0eb02919269f45ca6
mauriciomenon <54405514+mauriciomenon@users.noreply.github.com>
2024-10-19 22:51:31 -0300

    ...

--- URLShortener/dados_Teams.py
+++ URLShortener/dados_Teams.py
@@ -1,9 +1,12 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
-import time
 
 # Configuração do driver do Chrome para reutilizar o perfil do usuário
 chrome_options = Options()
@@ -14,15 +17,18 @@ chrome_options.add_argument("--profile-directory=Default")  # Substituir se voc�
 driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=chrome_options)
 
 # URL da reunião do Teams
-#meeting_url = "https://teams.microsoft.com/l/meetup-join/19%3Ameeting_NWQ5MmUyZWQtMjIwYy00YzU1LWE4YmUtZDA3NjM4OTU2MDg1%40thread.v2/0?context=%7B%22Tid%22%3A%223590422d-8e59-4036-9245-d6edd8cc0f7a%22%2C%22Oid%22%3A%22fe62638f-2f4f-41cc-b20f-dc91c9c4250d%22%7D"
 meeting_url = "https://teams.microsoft.com/l/meetup-join/19%3a663835a62f3f4662bf6577467faeb993%40thread.tacv2/1610834377647?context=%7b%22Tid%22%3a%2230b1e5ea-e05e-4a77-99d3-a3c62c328784%22%2c%22Oid%22%3a%227355f150-d4d3-4e4c-9800-bdcedf2ff88c%22%7d"
+
 try:
     # Abrir o navegador e acessar a página da reunião
     driver.get(meeting_url)
-    
-    # Aguardar alguns segundos para a página carregar completamente
-    time.sleep(10)  # Ajustar conforme necessário ou substituir por esperas explícitas
 
+    # Usar espera explícita para garantir que a página está carregada
+    wait = WebDriverWait(driver, 20)
+    
+    # Esperar que o título da página seja carregado
+    wait.until(EC.presence_of_element_located((By.TAG_NAME, "title")))
+    
     # Extrair o conteúdo da página
     page_source = driver.page_source
 
@@ -31,7 +37,7 @@ try:
 
     # Extrair o título da reunião
     title = soup.find('title').text.strip() if soup.find('title') else 'Título não encontrado'
-    
+
     # Exemplo para buscar o horário da reunião - ajustar conforme a estrutura real da página
     # Aqui estamos assumindo que a hora pode estar em um elemento específico
     time_info = soup.find('div', class_='some-time-class')  # Ajuste a classe ou tag correta
